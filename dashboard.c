589c34e9001eef6fefa83fe0aecbef895f6691f6
Nitesh8301 <65882845+Nitesh8301@users.noreply.github.com>
2020-10-01 17:42:27 +0530

    Authentication while login

--- dashboard.c
+++ dashboard.c
@@ -35,3 +35,58 @@ label:
     printf("Succesfully Registered!\n");
     i++;
 }
+//function to login
+void login()
+{
+    char temp[50], temp1[50];
+    int us = 0;
+    printf("Enter valid username\n");
+    scanf("%s", &temp);
+    for (int i = 0; i < 20; i++)
+    {
+        if (strcmp(temp, users[i].username) == 0)
+        {
+            us = 1;
+            printf("Enter password:\n");
+            scanf("%s", &temp1);
+            if (strcmp(temp1, users[i].password) == 0)
+            {
+                printf("Login Succesfull.\n");
+                // i=0;
+                return 0;
+            }
+            else
+            {
+                printf("Wrong password!\n");
+                login();
+            }
+        }
+    }
+    if(us != 1)
+    {
+        login();
+    }
+}
+void authenticate()
+{
+    int temp;
+    int i = 1;
+    while (i)
+    {
+        printf("Enter 1 to Signup\nEnter 2 for login\n");
+        scanf("%d", &temp);
+        switch (temp)
+        {
+        case 1:
+            signup();
+            break;
+        case 2:
+            login();
+            i = 0;
+            break;
+
+        default:
+            exit(0);
+        }
+    }
+}
